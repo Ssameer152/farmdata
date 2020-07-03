f0c16f0e26432723c493462ef8eb44947f888d1f
Aman Khandelwal <amank@oakstree.com>
2020-07-03 08:49:33 +0530

    nav and footer separated

--- index.php
+++ index.php
@@ -10,27 +10,8 @@ session_start();
     </head>
     
     <body>
-        <nav class="navbar navbar-dark navbar-expand-sm fixed-top bg-success">
-		<div class="container">
-			<a class="navbar-brand" href="index.php">FarmDB</a>
-			<div class="collapse navbar-collapse" id="Navbar">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item"><a class="nav-link" href="#"><span class="fa fa-home fa-lg"></span> Areas</a></li>
-					<li class="nav-item"><a class="nav-link" href="./aboutus.html"><span class="fa fa-info fa-lg"></span> People</a></li>
-					<li class="nav-item"><a class="nav-link" href="#"><span class="fa fa-list fa-lg"></span> Assets</a></li>
-					<li class="nav-item"><a class="nav-link" href="./contactus.html"><span class="fa fa-address-card fa-lg"></span> Logs</a></li>
-				</ul>
-				<span class="navbar-text">
-					<a href="logout.php">
-						<span class="fa fa-sign-in"></span> Logout
-					</a>
-				</span>
-			</div>
-		</div>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar"><span class="navbar-toggler-icon"></span></button>
-		</nav>
-		
-		<header class="jumbotron"></header>
+
+<?php include_once 'nav.php'; ?>
 	
 		<div class="container">
 			<div class="row">
@@ -70,8 +51,8 @@ _END;
 		</div>
 
         
-        <script src="js/jquery.min.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+<?php
+include_once 'foot.php';
+?>
     </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,21 @@
+    <nav class="navbar navbar-dark navbar-expand-sm fixed-top bg-success">
+		<div class="container">
+			<a class="navbar-brand" href="index.php">FarmDB</a>
+			<div class="collapse navbar-collapse" id="Navbar">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item"><a class="nav-link" href="#"><span class="fa fa-home fa-lg"></span> Areas</a></li>
+					<li class="nav-item"><a class="nav-link" href="./aboutus.html"><span class="fa fa-info fa-lg"></span> People</a></li>
+					<li class="nav-item"><a class="nav-link" href="#"><span class="fa fa-list fa-lg"></span> Assets</a></li>
+					<li class="nav-item"><a class="nav-link" href="./contactus.html"><span class="fa fa-address-card fa-lg"></span> Logs</a></li>
+				</ul>
+				<span class="navbar-text">
+					<a href="logout.php">
+						<span class="fa fa-sign-in"></span> Logout
+					</a>
+				</span>
+			</div>
+		</div>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar"><span class="navbar-toggler-icon"></span></button>
+	</nav>
+		
+		<header class="jumbotron"></header>
