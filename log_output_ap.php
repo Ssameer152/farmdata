b2643f26e4b6287f1b5bb2412eab0c52c20c913b
Aman Khandelwal <amank@oakstree.com>
2020-07-25 11:51:49 +0530

    Activity Ouput records added

---
+++ log_output_ap.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+if(isset($_SESSION['user']))
+{
+    include_once 'db.php';
+    
+    if(isset($_POST['area']) && $_POST['area']!='' && isset($_POST['qty']) && $_POST['qty']!='' && isset($_POST['logid']) && $_POST['logid']!='' && isset($_POST['person']) && $_POST['person']!='')
+    {
+        $area = mysqli_real_escape_string($db,$_POST['area']);
+        $qty = mysqli_real_escape_string($db,$_POST['qty']);
+        $logid = mysqli_real_escape_string($db,$_POST['logid']);
+        $person = mysqli_real_escape_string($db,$_POST['person']);
+        
+        $q = "INSERT INTO log_output(logid,resourceid,qty,person) VALUES('$logid','$area','$qty','$person')";
+        $r = mysqli_query($db,$q);
+        
+        $msg = "Log Added";
+        echo <<<_END
+        <meta http-equiv='refresh' content='0;url=log_output.php?logid=$logid&msg=$msg'>
+_END;
+    }
+    else
+    {
+        $msg = "Please select all the fields";
+        echo <<<_END
+        <meta http-equiv='refresh' content='0;url=logs.php?msg=$msg'>
+_END;
+    }
+}
+else
+{
+    $msg = "Please login!";
+    echo <<<_END
+    <meta http-equiv='refresh' content='0;url=index.php?msg=$msg'>
+_END;
+}
+
+?>
