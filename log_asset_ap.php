e65968c2c97b073b89323a5449fad6345f108fe6
Aman Khandelwal <amank@oakstree.com>
2020-08-03 08:46:05 +0530

    asset logs added

---
+++ log_asset_ap.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+if(isset($_SESSION['user']))
+{
+    include_once 'db.php';
+    
+    if(isset($_POST['area']) && $_POST['area']!='' && isset($_POST['qty']) && $_POST['qty']!='' && isset($_POST['logid']) && $_POST['logid']!='' && isset($_POST['person']) && $_POST['person']!='' && isset($_POST['comments']))
+    {
+        $asset = mysqli_real_escape_string($db,$_POST['area']);
+        $qty = mysqli_real_escape_string($db,$_POST['qty']);
+        $logid = mysqli_real_escape_string($db,$_POST['logid']);
+        $person = mysqli_real_escape_string($db,$_POST['person']);
+        $comments = mysqli_real_escape_string($db,$_POST['comments']);
+        
+        $q = "INSERT INTO log_assets(logid,assetid,usage_time,person,comments) VALUES('$logid','$asset','$qty','$person','$comments')";
+        $r = mysqli_query($db,$q);
+        
+        $msg = "Log Added";
+        echo <<<_END
+        <meta http-equiv='refresh' content='0;url=log_asset.php?logid=$logid&msg=$msg'>
+_END;
+    }
+    else
+    {
+        $msg = "Please select all the fields";
+        echo <<<_END
+        <meta http-equiv='refresh' content='0;url=logs.php?msg=$msg'>
+_END;
+    }
+}
+else
+{
+    $msg = "Please login!";
+    echo <<<_END
+    <meta http-equiv='refresh' content='0;url=index.php?msg=$msg'>
+_END;
+}
+
+?>
