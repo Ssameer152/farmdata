f75c00520f796332ec3f7a8aa00e3db0ce6d0265
Aman Khandelwal <amank@oakstree.com>
2020-08-29 10:51:07 +0530

    option to modify assets in log added

--- log_asset_ap.php
+++ log_asset_ap.php
@@ -13,10 +13,21 @@ if(isset($_SESSION['user']))
         $person = mysqli_real_escape_string($db,$_POST['person']);
         $comments = mysqli_real_escape_string($db,$_POST['comments']);
         
-        $q = "INSERT INTO log_assets(logid,assetid,usage_time,person,comments) VALUES('$logid','$asset','$qty','$person','$comments')";
-        $r = mysqli_query($db,$q);
         
-        $msg = "Log Added";
+        if(isset($_POST['mid']) && $_POST['mid']!='')
+        {
+            $mid = $_POST['mid'];
+            $q = "UPDATE log_assets SET assetid='$asset',person='$person',usage_time='$qty',comments='$comments' WHERE id='$mid' LIMIT 1";
+            $r = mysqli_query($db,$q);
+            
+            $msg = 'Updated';
+        }
+        else{
+            $q = "INSERT INTO log_assets(logid,assetid,usage_time,person,comments) VALUES('$logid','$asset','$qty','$person','$comments')";
+            $r = mysqli_query($db,$q);
+            
+            $msg = "Log Added";
+        }
         echo <<<_END
         <meta http-equiv='refresh' content='0;url=log_asset.php?logid=$logid&msg=$msg'>
 _END;
