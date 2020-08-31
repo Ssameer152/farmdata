04331fb79ef3d606d0e745896ddf4820ca92869d
Aman Khandelwal <amank@oakstree.com>
2020-08-31 20:41:57 +0530

    fix

--- log_resource.php
+++ log_resource.php
@@ -120,7 +120,20 @@ echo <<<_END
                         </div>
                         <div class="form-group">
                             <label for="Cost">Cost Per Unit(if resource is measured in kg then cost of 1 kg)</label>
-                            <input type="text" name="cpu" value="0.00" class="form-control">
+_END;
+
+if($db_cpu == ''){
+    echo <<<_END
+       <input type="text" name="cpu" value="0.00" class="form-control"> 
+_END;
+}
+else{
+    echo <<<_END
+      <input type="text" name="cpu" value="$db_cpu" class="form-control">
+_END;
+}
+
+echo <<<_END
                         </div>
                         <input type="hidden" name="logid" value="$logid">
 _END;
