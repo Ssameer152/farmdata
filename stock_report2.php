b9fcaa6b0bce506995684e3e4fe074504a30a1a6
Sameer Agarwal <asamy840@gmail.com>
2020-10-14 19:59:43 +0530

    stock report beta updated

--- stock_report2.php
+++ stock_report2.php
@@ -1,16 +1,17 @@
 <?php
 session_start();
-function getDimensionValue($db,$table,$gid,$name){
+function getDimensionValue($db, $table, $gid, $name)
+{
     $q = "SELECT * FROM $table WHERE id=$gid";
-    $r = mysqli_query($db,$q);
-    
+    $r = mysqli_query($db, $q);
+
     $res = mysqli_fetch_assoc($r);
-    
+
     $value = $res[$name];
-    
+
     return $value;
 }
-if(isset($_SESSION['user'])){
+if (isset($_SESSION['user'])) {
     include_once 'db.php';
     echo <<<_END
     <html>
@@ -23,8 +24,32 @@ if(isset($_SESSION['user'])){
         
         <body>    
 _END;
-include_once 'nav.php';
+    include_once 'nav.php';
     echo <<<_END
+        <div class="container">
+        <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="h2">Inventory</h2><br>
+                    <form action="stock_report2.php" method="get">
+                            <div class="row">
+                            <div class="col-lg">
+                                <input type="date" class="form-control" name="start_date">
+                            </div>
+                            <div class="col-lg">
+                            <input type="date" class="form-control" name="end_date">
+                        </div>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Show inventory</button>
+                    </form>
+                </div>
+                </div>
+                </div>
+_END;
+    if (isset($_GET['start_date']) && $_GET['start_date'] != '' && isset($_GET['end_date']) && $_GET['end_date'] != '') {
+        $start_date = mysqli_real_escape_string($db, $_GET['start_date']);
+        $end_date = mysqli_real_escape_string($db, $_GET['end_date']);
+        echo <<<_END
         <div class="container">
         <div class="row">
         <div class="col-lg-12">
@@ -42,53 +67,61 @@ include_once 'nav.php';
         </thead>
         <tbody>
 _END;
-    $q="SELECT * from resources where is_deleted=0";
-    $r=mysqli_query($db,$q);
-    while($res=mysqli_fetch_assoc($r)){
-        $id=$res['id'];
-        $resource=$res['resourcename'];
-        $unit=$res['unit'];
-        echo <<<_END
+        $produced1 = 0;
+        $consumed1 = 0;
+        $purchase1 = 0;
+        $q = "SELECT * from resources where is_deleted=0";
+        $r = mysqli_query($db, $q);
+        while ($res = mysqli_fetch_assoc($r)) {
+            $id = $res['id'];
+            $resource = $res['resourcename'];
+            $unit = $res['unit'];
+            echo <<<_END
         <tr>
         <td>$id</td>
-        <td>$resource</td>     
+        <td>$resource</td>  
+        
+        
 _END;
-        $q1="SELECT t.qty ,COALESCE(sum(t.qty),0) as q1 from (select qty from log_resource where resourceid='$id' and is_deleted=0) as t";
-        $r1=mysqli_query($db,$q1);
-        while($res1=mysqli_fetch_assoc($r1)){
-            $consumed=$res1['q1'];
+           
+            $q5 = "SELECT r.id, a.resourceid,b.resourceid,c.resourceid,sum(a.produced) as produced, sum(b.consumed) as con, sum(c.purchase) as pur  from resources r
+            LEFT JOIN (select resourceid,sum(qty) as produced from log_output where logid in (SELECT id FROM `logs` where cast(doe as date)>='$start_date' and TIMESTAMP(cast(doe as date))<='$end_date' and is_deleted=0) and is_deleted=0 GROUP BY resourceid) a on r.id = a.resourceid
+            LEFT JOIN (select resourceid,sum(qty) as consumed from log_resource where  logid in (SELECT id FROM `logs` where cast(doe as date)>='$start_date' and TIMESTAMP(cast(doe as date))<='$end_date' and is_deleted=0) and is_deleted=0 GROUP BY resourceid) b on r.id = b.resourceid
+            LEFT JOIN (select resourceid,sum(qty) as purchase from purchase_items where TIMESTAMP(cast(doe as date))>='$start_date' and TIMESTAMP(cast(doe as date))<='$end_date' and is_deleted=0 GROUP BY resourceid) c on r.id = c.resourceid where id='$id'";
+
+            $r1 = mysqli_query($db, $q5);
+
+            while ($res1 = mysqli_fetch_assoc($r1)) {
+                $produced = $res1['produced'];
+                $consumed = $res1['con'];
+                $purchase = $res1['pur'];
+
+                $produced1 += $produced;
+                $consumed1 += $consumed;
+                $purchase1 += $purchase;
+                $left = ($produced + $purchase) - $consumed;
                 echo <<<_END
                 <td>$consumed $unit</td>
 _END;
-    }
-        $q2="SELECT t.qty,COALESCE(sum(t.qty),0) as q2 from (select qty from purchase_items where resourceid='$id' and is_deleted=0) as t";
-        $r2=mysqli_query($db,$q2);
-        while($res2=mysqli_fetch_assoc($r2)){
-            $purchase=$res2['q2'];
-            echo <<<_END
+                echo <<<_END
             <td>$purchase $unit</td>
 _END;
-        }
-        $q3="SELECT t.qty ,COALESCE(sum(t.qty),0) as q3 from (select qty from log_output where resourceid='$id' and is_deleted=0) as t";
-        $r3=mysqli_query($db,$q3);
-        while($res3=mysqli_fetch_assoc($r3)){
-            $produced=$res3['q3'];
-            $left=$purchase+$produced-$consumed;
-            echo <<<_END
+                echo <<<_END
             <td>$produced $unit</td>
             <td>$left $unit</td>
             </tr>
+            
 _END;
+            }
         }
-    }
-    echo <<<_END
-        
-_END;
-    
-    
-        
+        echo <<<_END
    
-include_once 'foot.php';
+_END;
+    } else {
+        echo 'No items';
+    }
+
+    include_once 'foot.php';
     echo <<<_END
             </tbody>
         </table>
@@ -98,12 +131,9 @@ include_once 'foot.php';
         </body>
         </html>
 _END;
-}
-else
-{
+} else {
     $msg = "Please Login";
     echo <<<_END
     <meta http-equiv='refresh' content='0;url=index.php?msg=$msg'>
 _END;
 }
-?>
